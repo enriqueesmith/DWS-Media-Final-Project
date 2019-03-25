182acf18eff618173fa099e29279aa1c84fdfa6c
Enrique Smith <enriqueesmith@gmail.com>
2019-03-25 16:30:19 +0000

    EES Navbar Update

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
 export class Navbar extends React.Component {
 	render() {
 		return (
 			<nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
-				<a className="navbar-brand" href="#">
-					<i className="fas fa-book-open mr-2" />
-					DWS Media
-				</a>
+				<div className="leftHeader">
+					<a className="navbar-brand" href="#">
+						<i className="fas fa-book-open mr-2" />
+						DWS Media
+					</a>
+				</div>
 				<button
 					className="navbar-toggler"
 					type="button"
@@ -19,59 +22,62 @@ export class Navbar extends React.Component {
 					aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon" />
 				</button>
-
 				<div
 					className="collapse navbar-collapse"
 					id="navbarSupportedContent">
 					<ul className="navbar-nav mr-auto">
-						<li className="nav-item active">
-							<Link className="nav-link" to="/">
-								Home
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="#">
-								About Us
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="#">
-								Portfolio
-							</Link>
-						</li>
-						<li className="nav-item dropdown">
-							<a
-								className="nav-link dropdown-toggle"
-								id="navbarDropdown"
-								role="button"
-								data-toggle="dropdown"
-								aria-haspopup="true"
-								aria-expanded="false">
-								Packages and Add-Ons
-							</a>
-							<div
-								className="dropdown-menu"
-								aria-labelledby="navbarDropdown">
-								<Link className="dropdown-item" to="#">
-									Packages
+						<div className="centerHeader">
+							<li className="nav-item active">
+								<Link className="nav-link" to="/">
+									Home
 								</Link>
-								<div className="dropdown-divider" />
-								<Link className="dropdown-item" to="#">
-									Invidual Add-Ons
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link" to="#">
+									About Us
 								</Link>
-							</div>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="#">
-								<i className="fas fa-user-circle mr-3!" /> Login
-								or Sign Up
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="#">
-								<i className="fas fa-shopping-cart" />
-							</Link>
-						</li>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link" to="#">
+									Portfolio
+								</Link>
+							</li>
+							<li className="nav-item dropdown">
+								<a
+									className="nav-link dropdown-toggle"
+									id="navbarDropdown"
+									role="button"
+									data-toggle="dropdown"
+									aria-haspopup="true"
+									aria-expanded="false">
+									Packages and Add-Ons
+								</a>
+								<div
+									className="dropdown-menu"
+									aria-labelledby="navbarDropdown">
+									<Link className="dropdown-item" to="#">
+										Packages
+									</Link>
+									<div className="dropdown-divider" />
+									<Link className="dropdown-item" to="#">
+										Invidual Add-Ons
+									</Link>
+								</div>
+							</li>
+						</div>
+						<div className="rightHeader">
+							<li className="nav-item">
+								<Link className="nav-link" to="#">
+									<i className="fas fa-user-circle mr-3!" />{" "}
+									Login or Sign Up
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link" to="#">
+									<i className="fas fa-shopping-cart" />
+								</Link>
+							</li>
+						</div>
 					</ul>
 				</div>
 			</nav>
